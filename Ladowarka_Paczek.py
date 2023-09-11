f19309cb46cfa13622959a5fe7fed4ff1d017269
Marcin <chlopcyk85@gmail.com>
2023-09-11 19:02:55 +0200

    utworzenie potrzebnych petli do poprawnego dzialania algorytmu

--- Ladowarka_Paczek.py
+++ Ladowarka_Paczek.py
@@ -5,3 +5,14 @@ waga_min = 20
 paczka_min = 0
 
 ilosc_paczek = int(input("Ile elementów chcesz wysłać? "))
+if ilosc_paczek < 1:
+    paczki -= 1
+    print("Paczek musi być conajmniej 1 szt.")
+
+for i in range(ilosc_paczek):
+    while True:
+        waga = int(input("Podaj wagę każdego elementu do wysłania: "))
+        if waga < 1 or waga > 10:
+            print("Waga paczki nieprawidłowa, elementy mogą ważyć od 1 kg do max 10 kg \n")
+            print("Dodawanie paczek zostaje przerwane, a wszystkie paczki zostały wysłane\n")
+            break
